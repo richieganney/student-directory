f3ae6cced720aef1a733f7232a6ceb4ee963f061
Richie <richieganney@gmail.com>
2019-06-12 13:28:15 +0100

    directory.rb updated with user input

--- directory.rb
+++ directory.rb
@@ -1,19 +1,44 @@
 #first we print off the list of students.
 
-student_count = 11
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+def print_header
+  puts 'The students of Villains Academy'
+  puts "-------------"
+end
+def print(students)
+    students.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great student(s)."
+end
+def input_students
+    puts 'Please enter the name of the students'
+    puts 'To finish, just hit return twice'
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} student"
+      name = gets.chomp
+    end
+      students
+  end
 
-puts 'The students of Villains Academy'
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally, we print the total number of students
-puts "Overall, we have #{student_count} great students."
+students = input_students
+print_header
+print(students)
+print_footer(students)
