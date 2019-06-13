0d8286a1b26ad55d89791079dd32269198f62b86
Richie <richieganney@gmail.com>
2019-06-13 17:44:43 +0100

    final draft of scrpit. needs refactoring and additional functionality

--- directory.rb
+++ directory.rb
@@ -17,6 +17,8 @@ students = [
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit" # 9 because we'll be adding more items  
 end
 def process(selection)
@@ -25,6 +27,10 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -34,24 +40,24 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
    end
 end
 
 def input_students
   puts 'Please enter the name of the students, followed by their cohort month, hobbies and place of birth.'
   puts 'To finish, just hit return five times.'
-  name = gets.chomp
-  cohort = gets.chomp
-  hobby = gets.chomp
-  birth_place = gets.chomp
+  name = STDIN.gets.chomp
+  cohort = STDIN.gets.chomp
+  hobby = STDIN.gets.chomp
+  birth_place = STDIN.gets.chomp
   while true do
     @students << {name: name, cohort: cohort.to_sym, hobbies: hobby, country_of_birth: birth_place }
     puts @students.count > 1 ? "Now we have #{@students.count} students" : "Now we have #{@students.count} student"
-    name = gets.chomp
-    cohort = gets.chomp
-    hobby = gets.chomp
-    birth_place = gets.chomp
+    name = STDIN.gets.chomp
+    cohort = STDIN.gets.chomp
+    hobby = STDIN.gets.chomp
+    birth_place = STDIN.gets.chomp
       break if name.empty?
   end
 end
@@ -59,7 +65,7 @@ end
 def print_students_list
   puts "Enter a cohort month to print out the students belonging to that cohort."
   puts "Just type 'exit' then press enter to exit."
-    while (cohort_input = gets.chomp)
+    while (cohort_input = STDIN.gets.chomp)
       puts "Enter cohort month:"
       @students.each_with_index { |student, index|
       if student[:cohort].to_s.downcase == cohort_input.downcase && student[:name].length < 12
@@ -83,7 +89,36 @@ def show_students
   print_students_list
   print_footer
 end
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+  student_data = [student[:name], student[:cohort], student[:hobbies], student[:country_of_birth]]
+  csv_line = student_data.join(", ")
+  file.puts csv_line
+  end
+  file.close
+end
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort, hobbies, country_of_birth = line.chomp.split(',')
+  @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies.to_sym, country_of_birth: country_of_birth}
+  end
+    file.close
+end
+def try_load_students
+  filename = ARGV.first# first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
 
+try_load_students
 interactive_menu
-students = input_students
-show_students
