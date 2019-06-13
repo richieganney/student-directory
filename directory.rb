c47569d93a572cac70be4b04bc687167e621412d
Richie <richieganney@gmail.com>
2019-06-13 13:31:32 +0100

    updated directory.rb - interactive menu with local variable @students

--- directory.rb
+++ directory.rb
@@ -13,48 +13,77 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november, hobbies: "Being unliked by everyone", country_of_birth: :Westeros},
   {name: "Norman Bates", cohort: :november, hobbies: "Psycho", country_of_birth: :USA}
 ]
+@students = []
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items  
+end
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+end
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+   end
+end
+
 def input_students
-    puts 'Please enter the name of the students, followed by their hobbies, followed by their place of birth.'
-    puts 'To finish, just hit return four times.'
-    students = []
+  puts 'Please enter the name of the students, followed by their cohort month, hobbies and place of birth.'
+  puts 'To finish, just hit return five times.'
+  name = gets.chomp
+  cohort = gets.chomp
+  hobby = gets.chomp
+  birth_place = gets.chomp
+  while true do
+    @students << {name: name, cohort: cohort.to_sym, hobbies: hobby, country_of_birth: birth_place }
+    puts @students.count > 1 ? "Now we have #{@students.count} students" : "Now we have #{@students.count} student"
     name = gets.chomp
+    cohort = gets.chomp
     hobby = gets.chomp
     birth_place = gets.chomp
-    while true do
-      students << {name: name, cohort: :november, hobbies: hobby, country_of_birth: birth_place }
-      puts "Now we have #{students.count} student(s)"
-      name = gets.chomp
-      hobby = gets.chomp
-      birth_place = gets.chomp
-        break if name.empty?
-    end
-      students
+      break if name.empty?
   end
+end
+
+def print_students_list
+  puts "Enter a cohort month to print out the students belonging to that cohort."
+  puts "Just type 'exit' then press enter to exit."
+    while (cohort_input = gets.chomp)
+      puts "Enter cohort month:"
+      @students.each_with_index { |student, index|
+      if student[:cohort].to_s.downcase == cohort_input.downcase && student[:name].length < 12
+        puts "#{index +1}. #{student[:name].capitalize} (#{student[:cohort].capitalize} cohort). Hobbies include #{student[:hobbies]}. Born in #{student[:country_of_birth].capitalize}.".center(100)
+        puts "Enter cohort month:"
+      end
+      }
+    break if cohort_input == 'exit'
+    end
+end
 
 def print_header
   puts 'The students of Villains Academy'
   puts "-------------"
 end
-def print(students)
-    puts "Enter a letter to print names that begin with that letter."
-    puts "To exit, just press enter when promted for a letter."
-    while (first_letter = gets.chomp)
-    puts "Enter letter:"
-      students.each_with_index { |student, index|
-          if first_letter.downcase ==  student[:name].downcase[0] && student[:name].length < 12
-            puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort). Hobbies include #{student[:hobbies]}. Born in #{student[:country_of_birth]}."
-            puts "Enter letter:"
-          end
-      }
-    break if first_letter.empty?
-    end
-  end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great student(s)."
+def print_footer
+  puts "Overall, we have #{@students.count} great student(s)."
+end
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
+interactive_menu
 students = input_students
-print_header
-print(students)
-print_footer(students)
+show_students
